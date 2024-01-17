c9713523d11f99ed1971ec12b5eb93fb18f55600
Tejanin <theexama123@gmail.com>
2024-01-17 14:45:09 -0400

    Carousels, login, carrito y ordenes

---
+++ Server/ComeYaAPI/Services/PaginationService.cs
@@ -0,0 +1,19 @@
+﻿using ComeYaAPI.Models.DTOs.ItemDTOs;
+
+namespace ComeYaAPI.Services
+{
+    public class PaginationService
+    {
+        public IEnumerable<ReadItemDTO> Paginate(IEnumerable<ReadItemDTO> input, int page, decimal recordPerPage)
+        {
+            if (page == 0) return input;
+
+            var records = input
+                .Skip((page - 1) * ((int)recordPerPage))
+                .Take((int)recordPerPage)
+                .ToList();
+
+            return records;
+        }
+    }
+}
