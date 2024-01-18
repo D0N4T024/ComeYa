e359500a93d6842b27777c46620929c90baeac13
Tejanin <theexama123@gmail.com>
2024-01-18 02:52:14 -0400

    addItem

--- Client/come-ya/src/components/ProductView/ProductView.js
+++ Client/come-ya/src/components/ProductView/ProductView.js
@@ -5,8 +5,22 @@ import styles from './ProductView.module.css'
 import {AddShoppingCartIcon, FormControl, Select, MenuItem, Box }  from '@mui/material'
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import { useState } from 'react'
+import Apiservice from '@/Apiservice';
 
 const ProductView = ({ onClose, ...props }) => {
+  console.log(props)
+  const addItem = async (itemId, quantity) =>{
+    try {
+      
+      const response = await Apiservice.post('Cart/AddItem', {itemId,quantity});
+      console.log(response);
+      
+    } catch (error) {
+      console.error('Error al registrar usuario:', error);
+      
+      
+    }
+  }
   const toast = useToast()
   let theme = createTheme({
     palette: {
@@ -23,6 +37,7 @@ const ProductView = ({ onClose, ...props }) => {
       setQuantity(event.target.value);
   };
   const onSubmit = () => {
+    addItem(props.id,quantity)
     toast({
       title: 'Account created.',
       description: "We've created your account for you.",
